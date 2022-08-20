8d797ee4eacd7c29781c7d2ea982693b1488643c
thayeylolu <hamdallahhassanah@gmail.com>
2022-08-20 15:42:06 -0700

    implement inheritance of web driver

--- scrape.py
+++ scrape.py
@@ -9,7 +9,6 @@ Usage: scrape_tweet.py
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import datetime as dt
-import time
 import re
 
 from selenium import webdriver
@@ -25,12 +24,13 @@ today_date = dt.date.today().strftime("%Y-%m-%d")
 query_list = [
     (f"(@earthquakeBot) lang:en since:2022-08-18 until:{today_date})"),
 ]
+
 class WebDriverImplementation:
     def __init__(self):
         self.options = webdriver.ChromeOptions()
         self.options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
         self.driver = webdriver.Chrome(options = self.options)
-
+class GoogleMaps(WebDriverImplementation):
     def get_page(self, url):
         try:
             self.driver.get(url)
@@ -50,8 +50,9 @@ class WebDriverImplementation:
         self.driver.implicitly_wait(2)
         return [i.get_attribute('textContent') for i in elements]
 
+
 def main():
-    web_driver = WebDriverImplementation()
+    google_maps = GoogleMaps()
     tweets = []
     for query in query_list:
         for tweet in sntwitter.TwitterSearchScraper(query).get_items():
@@ -62,17 +63,20 @@ def main():
                 map_index = content.find("Map")
                 if content.find("Map:") != -1:
                     link_url = content[map_index + 5:]
-                    map_url = web_driver.get_page(link_url)
-                    coordinates = web_driver.scrape_coordinates()
-                    location = web_driver.scrape_location_details()      
+                    map_url = google_maps.get_page(link_url)
+                    coordinates = google_maps.scrape_coordinates()
+                    location_1 = google_maps.scrape_location_details()[0]  
+                    location_2 = google_maps.scrape_location_details()[1] 
+                    tweets.append([tweet.date, tweet.user.username,
+                                tweet.content, location_1, 
+                                location_2, map_url, coordinates]) 
                 else:
-                    location = ""
-                    map_url = ""
-                    coordinates = ""
-                tweets.append([tweet.date, tweet.user.username, tweet.content, location, map_url, coordinates])
-
+                    pass
+                
     df = pd.DataFrame(tweets, columns=["Date", "User", 
-                    "Tweet","Location", "Map URL", "Coordinates"])
+                                        "Tweet","Location",
+                                        "Location_2", "Map URL",
+                                         "Coordinates"])
 
     # to save to csv
     df.to_csv('earthquake_bot_with_location.csv')
