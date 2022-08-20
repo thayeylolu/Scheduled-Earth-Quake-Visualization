89a55a679f0c079dfc211b605e244c88823d9cb5
thayeylolu <hamdallahhassanah@gmail.com>
2022-08-20 16:36:48 -0700

    implement dictionary instead of list

--- scrape.py
+++ scrape.py
@@ -17,14 +17,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
 limit = 50
 timeout = 20
 today_date = dt.date.today().strftime("%Y-%m-%d")
 query_list = [
     (f"(@earthquakeBot) lang:en since:2022-08-18 until:{today_date})"),
 ]
-
 class WebDriverImplementation:
     def __init__(self):
         self.options = webdriver.ChromeOptions()
@@ -50,37 +48,55 @@ class GoogleMaps(WebDriverImplementation):
         self.driver.implicitly_wait(2)
         return [i.get_attribute('textContent') for i in elements]
 
+class USGSEarthQuake(WebDriverImplementation):
+    def get_page(self, url):
+        try:
+            self.driver.get(url)
+            quake_home = EC.url_contains("https:/earthquake.usgs.gov/")
+            WebDriverWait(self.driver, timeout).until(quake_home) # visit the last redirect link
+            return self.driver.current_url
+        except TimeoutException:
+            return "Timed out waiting for page to load"
 
 def main():
     google_maps = GoogleMaps()
-    tweets = []
+    tweets_dictionary = {}
+    tweet_key = 0
     for query in query_list:
         for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-            if len(tweets) > limit:
+            
+            if len(tweets_dictionary) > limit:
                 break
             else:
-                content = tweet.content
-                map_index = content.find("Map")
-                if content.find("Map:") != -1:
-                    link_url = content[map_index + 5:]
+                # print(tweet.content)
+                tweet_key += 1
+                find_link_url = re.search("(?<=Map: )(.*?)(?=$)", tweet.content)
+                # print(find_link_url)
+                if find_link_url is not None:
+                    link_url = find_link_url.group()
                     map_url = google_maps.get_page(link_url)
                     coordinates = google_maps.scrape_coordinates()
-                    location_1 = google_maps.scrape_location_details()[0]  
-                    location_2 = google_maps.scrape_location_details()[1] 
-                    tweets.append([tweet.date, tweet.user.username,
-                                tweet.content, location_1, 
-                                location_2, map_url, coordinates]) 
+                    location_1 = google_maps.scrape_location_details()[0]
+                    location_2 = google_maps.scrape_location_details()[1]
+                    magnitude = re.search("(?<=A )(.*?)(?= magnitude)", tweet.content).group()
+                    region =  re.search("(?<=occured )|(?<=earthquake)(.*?)(?=Details)", tweet.content).group()
+                    tweets_dictionary[tweet_key]= [tweet.date, tweet.user.username,
+                                                region, tweet.content, location_1,    
+                                                location_2, map_url, coordinates, magnitude]
+                    print(tweets_dictionary)
                 else:
                     pass
                 
-    df = pd.DataFrame(tweets, columns=["Date", "User", 
+    df = pd.DataFrame.from_dict(tweets_dictionary,orient='index',
+                                 columns=[
+                                        "Date", "User", 
+                                        "Region",
                                         "Tweet","Location",
                                         "Location_2", "Map URL",
-                                         "Coordinates"])
+                                        "Coordinates","Magnitude"])
 
     # to save to csv
-    df.to_csv('earthquake_bot_with_location.csv')
-
+    df.to_csv(f'earthquake_bot_{today_date}.csv')
 if __name__ == "__main__":
     main()
 
