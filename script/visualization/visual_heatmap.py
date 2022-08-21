3a0cfd9375b31ed6efa912b3c97dc09b50180066
thayeylolu <hamdallahhassanah@gmail.com>
2022-08-21 09:41:02 -0700

    feat: modify path and include append function

--- script/visualization/visual_heatmap.py
+++ script/visualization/visual_heatmap.py
@@ -1,6 +1,8 @@
 import time
+import datetime as dt
 import pandas as pd
 import os
+import glob
 from folium import plugins
 from folium.plugins import HeatMap
 import folium
@@ -8,29 +10,37 @@ import numpy as np
 from selenium import webdriver
 
 driver = webdriver.Firefox()
+today_date = dt.date.today().strftime("%Y-%m-%d")
+str_today_date = str(today_date).replace('-', '_')
 
 CURRENT_DIR = os.getcwd()
-PARENT_DIR = CURRENT_DIR.replace("/notebooks/visualizations", "")
-RAW_DATA_DIR = PARENT_DIR + "/data/raw"
-CLEAN_DATA_DIR = PARENT_DIR + "/data/preprocessed/clean"
-LOCATION_DATA_DIR = PARENT_DIR + "/data/preprocessed/location"
-NUMERIC_DATA_DIR = PARENT_DIR + "/data/preprocessed/numeric"
-RESULT_DIR = PARENT_DIR + "/results"
+PARENT_DIR = CURRENT_DIR.replace("/script/visualization", "")
+
+RAW_DATA_DIR = os.path.join(PARENT_DIR ,"/data/raw")
+CLEAN_DATA_DIR = os.path.join(PARENT_DIR, "data/preprocessed/clean")
+LOCATION_DATA_DIR = os.path.join(PARENT_DIR , "data/preprocessed/location")
+NUMERIC_DATA_DIR = os.path.join(PARENT_DIR , "data/preprocessed/numeric") 
+RESULT_DIR = os.path.join(PARENT_DIR, "results")
 
 if not os.path.exists(RESULT_DIR):
     os.mkdir(RESULT_DIR)
 
 # join the two data on pid
-num_df = pd.read_csv(f'{NUMERIC_DATA_DIR}/numeric_eq_2022_08_20.csv')
-loc_df = df = pd.read_csv(f'{LOCATION_DATA_DIR}/location_eq_2022_08_20.csv')
 
-num_df = num_df.drop("Unnamed: 0",axis=1)
-loc_df = loc_df.drop("Unnamed: 0",axis=1)
+def _append_files(directory, match_files):
+    files = os.path.join(directory, match_files)
+    files = glob.glob(files)
+    df = pd.concat(map(pd.read_csv, files), ignore_index=True)
+    df = df.drop("Unnamed: 0",axis=1)
+    return df
 
+num_df = _append_files(NUMERIC_DATA_DIR, "numeric_eq*.csv")
+loc_df = _append_files(LOCATION_DATA_DIR, "location_eq*.csv")
 merge_df = pd.merge(num_df, loc_df)
-merge_df['mag_norm'] = merge_df.magnitude/np.linalg.norm(merge_df.magnitude) # normalize data
-print(RESULT_DIR)
+
 def make_heat_map(merge_df):
+
+    merge_df['mag_norm'] = merge_df.magnitude/np.linalg.norm(merge_df.magnitude) # normalize data
     map = folium.Map(location = [15,30], tiles='Cartodb dark_matter', zoom_start = 1.5)
     heat_df = merge_df[['lat', 'long', 'mag_norm']]
     heat_data = [[row['lat'],row['long'], row['mag_norm']] for index, row in heat_df.iterrows()]
@@ -38,7 +48,7 @@ def make_heat_map(merge_df):
 
     map.save(f"{RESULT_DIR}/heat_map_eq.html")
     url = f"file://{RESULT_DIR}/heat_map_eq.html"
-    print(url)
+    #print(url)
     driver.get(url)
     time.sleep(5)
     driver.save_screenshot(f"{RESULT_DIR}/heat_map_eq.png")
